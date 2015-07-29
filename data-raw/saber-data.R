34a4c621eb18a4bf0baf69696aa0902e373ffb66
daniel <damorenoco@unal.edu.co>
2015-07-29 01:25:05 -0500

    Better strategy to set col_types. Now just edit the corresponding .csv file in data-raw/types for each file to properly read. Defaults set by readr.

--- data-raw/saber-data.R
+++ data-raw/saber-data.R
@@ -4,20 +4,15 @@ library("curl")
 
 # Get data:
 
-ftp_auth <- readLines(con = "data-raw/ftp_auth")
+ftp_auth <- readLines(con = file.path("data-raw", "ftp_auth"))
 server <- paste0("ftp://", ftp_auth, "@ftp.icfes.gov.co/SABER11/")
 
 con <- curl(server, "r", new_handle(dirlistonly = TRUE))
 
-server_files <- grep(pattern = ".zip", x = readLines(con), fixed = TRUE,
-                     value = TRUE)
+files <- grep(pattern = ".zip", x = readLines(con), fixed = TRUE, value = TRUE)
+file_name <- function(file){gsub("-", "_", substr(file, 1, 10), fixed = TRUE)}
 
 close(con)
-
-files_urls <- paste0(server, server_files)
-
-dest_files <- paste0("data-raw/raw/", server_files)
-names(dest_files) <- gsub("-", "_", substr(server_files, 1, 10), fixed = TRUE)
 
 download_if_not_exists <- function(url, destfile){
   if (!file.exists(destfile))
@@ -25,21 +20,40 @@ download_if_not_exists <- function(url, destfile){
   file.exists(destfile)
 }
 
-Map(download_if_not_exists, destfile = dest_files, url = files_urls)
+Map(download_if_not_exists, destfile = file.path("data-raw", "raw", files),
+    url = paste0(server, files))
 
 # Read data
+
+## Read or make file with column types
+get_types <- function(file, ...){
+  types_file <- file.path("data-raw", "types", paste0(file_name(file), ".csv"))
+
+  if (!file.exists(file.path(types_file))){
+    df <- read_delim(file.path("data-raw", "raw", file), del = "|",
+                     na = c("---", "-1", "", "            "), n_max = 100)
+
+    types_df <- data_frame(Variable = names(df), Tipo = sapply(df, typeof)) %>%
+      mutate(Tipo = ifelse(Tipo == "double", paste0("col_euro_", Tipo, "()"),
+                           paste0("col_", Tipo, "()")))
+
+    write_csv(types_df, types_file)
+    lapply(parse(text = types_df[["Tipo"]]), eval)
+  } else {
+    types_df <- read_csv(types_file)
+    lapply(parse(text = types_df[["Tipo"]]), eval)
+  }
+}
+
+columns <- lapply(files, get_types)
+
 read_save <- function(file, ...){
   df <- list(read_delim(file, ...))
-  names(df) <- names(file)
+  names(df) <- file_name(file)
 
   save(list = names(df), file = file.path("data", paste0(names(df), ".rda")),
        envir = as.environment(df), compress = "xz")
 }
 
-# Get pre-specified column types
-source("data-raw/column_types.R")
-
-for (i in seq_along(dest_files)){
-  file <- dest_files[i]
-  read_save(file, del = "|", col_types = columns[[i]])
-}
+Map(read_save, file = files, del = "|", col_types = columns,
+    na = c("---", "-1", "", "            "))
